6976faf3638326327aeb016acbb2fd7b0fcbc297
aryanagarwal15 <f2016705@goa.bits-pilani.ac.in>
2017-09-02 19:03:19 +0530

    Added Bottom Navigation Drawer

---
+++ app/src/main/java/org/bits_waves/waves2017/ItemFourFragment.java
@@ -0,0 +1,26 @@
+package org.bits_waves.waves2017;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ItemFourFragment extends Fragment {
+    public static ItemFourFragment newInstance() {
+        ItemFourFragment fragment = new ItemFourFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_item_four, container, false);
+    }
+}
---
+++ app/src/main/java/org/bits_waves/waves2017/ItemOneFragment.java
@@ -0,0 +1,26 @@
+package org.bits_waves.waves2017;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ItemOneFragment extends Fragment {
+    public static ItemOneFragment newInstance() {
+        ItemOneFragment fragment = new ItemOneFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_item_one, container, false);
+    }
+}
